9fce007ea09b065d094c4b9f0d41cbe22550f8f4
Pols97 <jeanpols97@gmail.com>
2020-09-27 00:04:19 +0200

    change_of_plans

--- sketch.js
+++ sketch.js
@@ -1,78 +1,84 @@
 
 
-let price;
-let step = 0;
-let direction = 1;
-let perimetert0 = 0;
+let roots = [];
+let numRoots = 20;
+let numRootsAdd = 4;
 let windowWidtht0 = 0;
 let windowHeightt0 = 0;
-let xCoin = 0;
-let yCoin = 0;
+let windowDiagonal = 0;
+let fr = 16; // frame rate
+let maxfr = fr*10;
+
+let stepLengthRange = [4,24]; // range of length of step
 
 function setup() {
   windowWidtht0 = windowWidth;
   windowHeightt0 = windowHeight-4;
-  perimetert0 = 2*(windowWidtht0+windowHeightt0);
+  windowDiagonal = sqrt((windowWidtht0 ** 2)+(windowHeightt0 ** 2));
   createCanvas(windowWidtht0, windowHeightt0);
   angleMode(DEGREES);
 
-  background(20);
+  background(0,12,40);
 
-  price = windowHeight/2;
-  frameRate(12);
+  frameRate(fr);
+
+  for (var i = 0; i < numRoots; i++) {
+    roots.push([0,0]);
+  }
+
+  push();
+  translate(windowWidtht0/2,windowHeightt0/2);
+  stroke(0,0,0,0);
+  fill(200,200,200,250);
+  textSize(48);
+  textFont('Georgia');
+  text('Rotating Stochastic Paths', -270, -windowHeightt0/4);
+  pop();
 }
 
 function draw() {
 
+  if(frameCount % maxfr == 1 && frameCount / maxfr <= 15) { // for 15 regens
+    for (var i = 0; i < numRootsAdd; i++) { // regen
+      roots.push([0,0]);
+    }
+  }
 
   push();
-  if(step*10>windowWidth)
-    direction = -1;
-  else if(direction == -1 && step < 1)
-    direction = 1;
-
-  step += direction;
-
-  fill(0,0,0,3);
-  stroke(0,0,0,3);
-  rect(0,0,windowWidth,windowHeight-4);
-
-  //translate(-10,0);
-  let delta = random(-50,50);
-  let newprice = price+delta;
-  if(newprice < 0){
-    newprice += 20;
-    delta += 20;
-  }
-  if(newprice > windowHeight){
-    newprice -= 20;
-    delta -= 20;
-  }
-  let quantile1 = random(5,50);
-  let quantile2 = random(0,quantile1/2);
-  let quantile3 = random(0,quantile1/2);
-  if(delta<0) {
-    fill(0,220,20);
-    stroke(0,220,20);
-  } else {
-    fill(240,20,0);
-    stroke(240,20,0);
-  }
-  line(step*10 + 3,newprice-quantile1/2-quantile2,step*10 + 3,newprice+quantile1/2+quantile3);
-  rect(step*10,newprice-quantile1/2,6,quantile1);
-  price = newprice;
+  translate(windowWidtht0/2,windowHeightt0/2);
+  fill(0,0,0,6);
+  let rc = (frameCount*32) % windowDiagonal; //(frameCount % (windowDiagonal ** 0.5))**2 % windowDiagonal;
+  if (rc > windowDiagonal)
+    rc = windowDiagonal;
+  stroke(50,50,50,55+200*(rc/windowDiagonal)**0.8);
+  strokeWeight(1.5);
+  ellipse(0,0,rc,rc);
   pop();
 
-	push();
-	translate(mouseX,mouseY);
-	rotate(0.1*frameCount);
-  stroke(0,0,0,0);
-  fill(240,220,0,100);
-  for (var iCoin = 0; iCoin < 2; iCoin++) {
-    let diameter = random(2,6);
-    let distance = random(0,8)*random(0,8);
-    let angle = random(1,360);
-    ellipse(xCoin+distance*cos(angle),yCoin+distance*sin(angle),diameter,diameter);
+  push();
+  let rs = (frameCount*32)%60;
+  translate(mouseX,mouseY);
+  rotate(frameCount*19);
+  fill(0,0,0,32);
+  stroke(250,255,100,100);
+  strokeWeight(rs/12);
+  if (! (mouseX == 0 && mouseY == 0)) // avoid annoying thing top left
+      rect(-rs/2,-rs/2,rs,rs);
+  pop();
+
+  push();
+  translate(windowWidtht0/2,windowHeightt0/2);
+  rotate(frameCount*4);
+  stroke(150+(127+frameCount*1.2)%105, frameCount%31, 100+(0.8*frameCount-79)%155);
+  let lr = roots.length;
+  for (var ir = 0; ir < lr; ir++) {
+    strokeWeight((ir+frameCount)%5);
+    let r = roots[ir];
+    let stepLength = ((ir+frameCount) % stepLengthRange[1]) + stepLengthRange[0];
+    let stepAngle = random(0,360);
+    let rf = [r[0]+stepLength*cos(stepAngle),r[1]+stepLength*sin(stepAngle)]; // end vertex
+    line(r[0],r[1],rf[0],rf[1]);
+    roots[ir] = rf;
   }
 	pop();
 }
